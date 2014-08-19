768c8ba8f837725d3701eb45e9e38fb339ab1f6f
prabhasp <prabhas.pokharel@gmail.com>
2014-08-19 09:52:22 +0545

    charts with x-axis (kind-of)

--- src/small_app/views.clj
+++ src/small_app/views.clj
@@ -5,13 +5,14 @@
   (:require [clojure.string :as str]
             [c2.scale :as scale]
             [clj-http.client :as client]
-            [c2.svg :as svg]))
+            [c2.svg :as svg]
+            [c2.ticks :as tick]))
 
 (defn gen-page-head
   [title]
     [:head
-       [:title (str "Small-app Homepage " title)]
-          (include-css "resources/public/css/styles.css")])
+       [:title (str "c2-experiments Homepage " title)]
+          (include-css "/css/style.css")])
 
 
 
@@ -28,25 +29,30 @@
                [(na-fix ((keyword field_xpath) data-item))
                 (:count data-item)]))))
 
-(defn- extract-data-for-histogram [chart-data]
+(defn- extract-data-for-histogram [chart-data bins]
   (let [{:keys [data field_xpath]} chart-data
         qn-key (keyword field_xpath)
         non-nil-data (remove (fn [el] (nil? (qn-key el))) data)
         data-with-numbers (map (fn [el]
                                  (assoc el qn-key (Integer. (qn-key el))))
                                non-nil-data)
-        binned-data (histogram data-with-numbers :value qn-key :range [0 60] :bins 16)]
+        domain (map qn-key data-with-numbers)
+        xmin (- (apply min domain) 1)
+        xmax (+ (apply max domain) 1)
+        binned-data (histogram data-with-numbers :value qn-key :bins bins)]
+
     (for [data-item binned-data]
       [(:x (meta data-item))
        (:dx (meta data-item))
        (apply + (map :count data-item))])))
 
-;(extract-data-for-histogram (data-for-qn "q8"))
+; (extract-data-for-histogram n-data)
+; (def chart-data n-data)
 
 (defn numeric-chart
   [chart-data]
-  (let [chart-width 700 chart-height 300 margin 50 small-margin 2
-        extracted-data (extract-data-for-histogram chart-data)
+  (let [chart-width 700 chart-height 300  bins 10
+        extracted-data (extract-data-for-histogram chart-data bins)
         {:keys [field_label field_xpath]} chart-data
         x-series (map first extracted-data)
         dx-series (map second extracted-data)
@@ -56,18 +62,35 @@
         x-scale (scale/linear :domain [xmin xmax]
                               :range [0 chart-width])
         y-scale (scale/linear :domain [0 (apply max y-series)]
-                              :range [0 chart-height])]
+                              :range [0 chart-height])
+        ticks (:ticks (tick/search [xmin xmax] :clamp? true))
+        margin 0 small-margin 2
+        bin-width (- (/ chart-width bins) small-margin)]
     [:table#histogram.table-bordered
      [:thead [:tr [:th field_label]]]
      [:tbody [:tr [:td
         [:svg {:width (+ margin chart-width) :height (+ margin chart-height)}
-         (unify extracted-data (fn [[x dx y]]
-                               [:g.bar {:transform (str "translate("
-                                                        (float (x-scale x)) ","
-                                                          (float (y-scale y)) ")")}
-                                [:rect {:x 1 :style "fill:grey"
-                                        :height (- chart-height (float (y-scale y)))
-                                        :width (- (x-scale dx) small-margin)}]]))]]]]]))
+         (unify extracted-data
+                (fn [[x dx y]]
+                  (let [x-scaled (float (x-scale x))
+                        y-scaled (float (y-scale y))]
+                    [:g
+                     [:g.bar {:transform (svg/translate [x-scaled (- chart-height y-scaled)])}
+                     [:rect {:x 1 :style "fill:grey"
+                             :height y-scaled
+                             :width bin-width}]]
+                    ])))
+         (unify ticks
+                (fn [t]
+                  [:g.tick {:transform
+                            (svg/translate [(float (x-scale t)) 20])}
+                   [:text (str t)]]))
+         (svg/axis x-scale ticks :orientation :bottom)
+         ]]]]]))
+; (numeric-chart n-data)
+;        [:g.axis {:transform (svg/translate [0 (- chart-height 50)])}
+;           (svg/axis x-scale ticks :orientation :bottom)]
+
 
 (defn category-chart [chart-data]
   (let [bar-max-width 500
@@ -95,13 +118,16 @@
 
 (defn data-for-qn [qn]
   (:body (client/get
-          (str "https://ona.io/api/v1/charts/196.json?field_name=" qn)
+          (str "http://localhost:3000/" qn ".json")
           {:as :json})))
+
+(def n-data (data-for-qn "q8"))
+(def c-data (data-for-qn "q18a"))
 
 (defn home-page
   []
   (html5
    (gen-page-head "Home")
    [:div [:div#container
-     (category-chart (data-for-qn "q18a"))
-     (numeric-chart (data-for-qn "q8"))]]))
+     (category-chart c-data)
+     (numeric-chart n-data)]]))
